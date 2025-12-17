87be921aacf985a204abf1ce29428e1f036992ac
GS0968 <gagan.s.govind@gmail.com>
2025-12-17 12:36:04 +0900

    Adding classes to the code
    Added a class called room where the rooms detailed will be showed

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import sys
 
-def start():
+def start(): #not fully complete yet
     option=input("Do you want to start a new game or load a previous game?: ")
     option=option.strip()
     option=option.lower()
@@ -15,6 +15,38 @@ def start():
         case "new" | "new game":
             newfile=input("Enter savefile name: ")
 
-class game():
-    def __init__(self,charnum=6,chartype):
-        
+class Room:
+    def __init__(self,name, items, monster, trap, visit=False):
+        self.rname=name 
+        self.loot=items #will get a list of each loot in the room
+        self.monsters=monster #will get a list of each monster(if visted the attacked mosters wont be seen) in the room
+        self.visited=visit
+        self._traps=trap #will show the number of traps present in the room (shouldnt be visible to characters/players)
+    
+    def showinfo(self):
+        name=self.rname
+        if len(self.loot)>0:
+            items=self.loot
+            for i in range(len(items)):
+                itemstring=str()
+                itemstring=itemstring+items[i]
+        if len(self.monsters)>0:
+            monsters=self.monsters
+            for i in range(len(monsters)):
+                monsterinfo=str()
+                monster=Monster(monsters[i])
+                monsterinfo=monsterinfo+monster.getinfo()
+        if self.visited==True:
+            visit="You have already visited this place"
+        else:
+            visit="You haven't opened this room"
+        if self._traps>5:
+            traps= "multiple traps"
+        elif 0<self._traps<=5:
+            traps= "a few traps"
+        else:
+            traps= "no traps"
+
+class Monster: #didn't start yet just added for class room
+    def __init__(self):
+        self
