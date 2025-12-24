56a529c8ff7f566cb60df44b115046fca34254dc
GS0968 <gagan.s.govind@gmail.com>
2025-12-24 15:52:26 +0900

    Update project.py

--- project.py
+++ project.py
@@ -24,7 +24,7 @@ class Room:
     def __init__(self,name, items, monster, trap, visit=False):
         self.rname=name 
         self.loot=items #will get a list of each loot in the room
-        self.monsters=monster #will get a list of each monster(if visted the attacked mosters wont be seen) in the room
+        self.monsters=monster #will get a list of each monster(if visted the defeated mosters wont be seen) in the room
         self.visited=visit
         self._traps=trap #will show the number of traps present in the room (shouldnt be visible to characters/players)
     
@@ -48,18 +48,36 @@ class Room:
                 monster=Monster(monsters[i])
                 monsterinfo=monsterinfo+monster.getinfo()
 
-class Monster: #didn't start yet just added for class room
-    def __init__(self, name):
+class Monster: 
+    def __init__(self, name, health, power, room):
         self.mname=name
-        #self.health=
-        #self.power=
+        self.health=health
+        self.power=power
+        self.room=room
     
-    def attack(self, hit):
-        #characters=getcharacters()
-        #character=random.choice(characters)
-        #characterinfo=gentinfo(character)
+    def attack(self):
+        characters=getcharacters()
+        character=random.choice(characters)
+        characterinfo=gentinfo(character)
+        c=Character(character, characterinfo)
+        c.takedamage(self.power)
+
+    def takedamage(self, damage):
+        health=self.health-damage
+        if health>0:
+            self.health=health
+        else:
+            print("You have defeated the monster")
+            removemonster(self.room)
+
 
 
 class Character:
-    def __init__(self, name):
+    def __init__(self, name, health, power, type):
         self.name=name
+        self.health=health
+        self.power=power
+        self.type=type
+
+    def attack(self,monster):
+        dice=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
