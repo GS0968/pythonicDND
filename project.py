b6a16b4d87d01951419ab21555b14fe63e7bb970
Faustrex-1 <prashun123bas@gmail.com>
2025-12-28 23:20:25 +0900

    Implement save and load game features
    Added functionality to save and load game state using JSON.

--- project.py
+++ project.py
@@ -1,20 +1,45 @@
 import sys
 import random
+import json
+
+def save_game(filename,player,rooms):
+    game_state = {
+        "player": player.to_dict(),
+        "rooms": [room.todict() for room in rooms]
+    }
+    with open(filename, "w") as file:
+        json.dump(game_state, file, indent=4)
+        
+def load_game(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    player = Character.from_dict(data["player"])
+    rooms = [Room.from_dict(r) for r in data["rooms"]]
+    return player, rooms
+
 
 def start(): #not fully complete yet
     option=input("Do you want to start a new game or load a previous game?: ")
-    option=option.strip()
-    option=option.lower()
+    option=option.strip().lower()
+    
     match option:
         case "load":
             oldfile=input("Enter the loadfile name: ")
             try:
-                open(oldfile, "r") # have to set a format in how the data will be stored and then do th rest
+                return load_game(oldfile)
             except FileNotFoundError:
                 sys.exit("Error: File Not Found")
 
         case "new" | "new game":
             newfile=input("Enter savefile name: ")
+            new_name = input("Enter the name for your Character")
+            player = Character(new_name,,,"")#add the starting values
+            rooms = [
+                Room()
+                Room()
+            ]
+            save_game(newfile, player, rooms)
+            return player, rooms
 
 def roll():
     return random.randint(1,20)
@@ -26,6 +51,25 @@ class Room:
         self.monsters=monster #will get a list of each monster(if visted the defeated mosters wont be seen) in the room
         self.visited=visit
         self._traps=trap #will show the number of traps present in the room (shouldnt be visible to characters/players)
+
+    def to_dict(self):
+        return {
+            "name": self.rname,
+            "loot": self.loot,
+            "monsters": [m.to_dict() for m in self.monsters],
+            "traps": self._traps,
+            "visited": self.visited
+        }
+        
+    def from_dict(cls, data):
+        monsters = [Monster.from_dict(m) for m in data["monsters"]]
+        return cls(
+            data["name"],
+            data["loot"],
+            monsters,
+            data["traps"],
+            data["visited"]
+        )
     
     def showinfo(self):
         name=self.rname
@@ -40,6 +84,7 @@ class Room:
 
         else:
             visit=f"You haven't opened this room. There are currently {len(self.monsters)} undefeated, {traps} armed, and {len(self.loot)} not looted in this place"
+       
         if len(self.monsters)>0:
             monsters=self.monsters
             monsterinfo=str()
@@ -53,6 +98,22 @@ class Monster:
         self.health=health
         self.power=power
         self.room=room
+
+    def to_dict(self):
+        return {
+            "name": self.mname,
+            "health": self.health,
+            "power": self.power,
+            "room": self.room
+        }
+
+    def from_dict(cls, data):
+        return cls(
+            data["name"],
+            data["health"],
+            data["power"],
+            data["room"]
+        )
     
     def attack(self):
         characters=getcharacters()
@@ -80,6 +141,22 @@ class Character:
         self.health=health
         self.power=power
         self.type=type
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "health": self.health,
+            "power": self.power,
+            "type": self.type
+        }
+
+    def from_dict(cls, data):
+        return cls(
+            data["name"],
+            data["health"],
+            data["power"],
+            data["type"]
+        )
 
     def attack(self,mdetails):
         attack=self.power*(roll()/10)
